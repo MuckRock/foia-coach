a416c8b78da113cfd28eab2aaed7b32e8c11c6cd
Chris Amico <eyeseast@gmail.com>
2026-04-16 21:40:28 -0400

    Add dj_database_url

--- api/config/settings/render.py
+++ api/config/settings/render.py
@@ -1,2 +1,12 @@
 # settings to run on Render
 from .base import *
+
+import dj_database_url
+
+DATABASES = {
+    "default": dj_database_url.parse(
+        "postgres://...",
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
+}
